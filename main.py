6756d3108e8d6af9a96baddfaed9933c70f72dad
ArpanSaha07 <sahaarpan550@gmail.com>
2023-01-21 21:06:19 -0500

    Commit-1

--- main.py
+++ main.py
@@ -9,3 +9,64 @@ font = pygame.font.Font('./assets/font/myFont.ttf', 32)
 WIDTH = 900
 HEIGHT = 800
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+bgs = []
+banners = []
+guns = []
+target_images = [[], [], []]
+targets = {1: [10, 5, 3],
+           2: [12, 8, 5],
+           3: [15,12, 8, 3]}
+level = 1
+for i in range(1, 4):
+    bgs.append(pygame.image.load(f'assets/bgs/{i}.png'))
+    banners.append(pygame.image.load(f'assets/banners/{i}.png'))
+    guns.append(pygame.transform.scale(pygame.image.load(f'assets/guns/{i}.png'), (100, 100)))
+    if i < 3:
+        for j in range(1, 4):
+            target_images[i - 1].append(pygame.transform.scale(pygame.image.load(f'assets/targets/{i}/{j}.png'), (120 -, 100)))
+
+def draw_gun():
+    mouse_pos = pygame.mouse.get_pos()
+    gun_point = (WIDTH/2, HEIGHT - 200)
+    lasers = ['red', 'purple', 'green']
+    clicks = pygame.mouse.get_pressed()
+    if mouse_pos[0] != gun_point[0]:            # to avoid zero division error
+        gradient = (mouse_pos[1] - gun_point[1])/ (mouse_pos[0] - gun_point[0])
+    else:
+        gradient = -10000000
+    angle = math.atan(gradient)
+    rotation = math.degrees(angle)
+
+    if mouse_pos[0] < WIDTH/2:      # if mouse pointer is in the left side
+        gun = pygame.transform.flip(guns[level - 1], True, False)
+        if mouse_pos[1] < 600:
+            screen.blit(pygame.transform.rotate(gun, 90 - rotation), (WIDTH/2 - 90, HEIGHT - 250))
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level - 1], mouse_pos, 5)
+
+    else:
+        gun = pygame.transform.flip(guns[level - 1], True, False)
+        if mouse_pos[1] < 600:
+            screen.blit(pygame.transform.rotate(gun, 270 - rotation), (WIDTH / 2 - 90, HEIGHT - 250))
+            if clicks[0]:
+                pygame.draw.circle(screen, lasers[level - 1], mouse_pos, 5)
+
+
+run = True
+while run:
+    timer.tick(fps)     #??
+
+    screen.fill('black')
+    screen.blit(bgs[level - 1], (0, 0))
+    screen.blit(banners[level - 1], (0, HEIGHT - 200))
+
+    if level > 0:
+        draw_gun()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.flip()
+
+pygame.quit()
